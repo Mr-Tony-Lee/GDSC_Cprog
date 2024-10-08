1f41d841e46b143b36dc17d011a51996c118f2b7
Mr-Tony-Lee <lchengyan49@gmail.com>
2024-10-09 00:29:30 +0800

    first commit

---
+++ C_part3_ifelse.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+    /*
+        if(boolean_expression_1){
+            當第一個表達式為真時會執行
+        }
+        else if( boolean_expression_2){
+            當第二個表達式為真時會執行
+        }
+        else {
+            當上面都不為真時會執行
+        }
+    */
+    
+}
