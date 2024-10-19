26dd4cc91116d1fd29a01146416249a4dfcaabf3
Mr-Tony-Lee <lchengyan49@gmail.com>
2024-10-20 00:34:15 +0800

    1020 commit

--- 20241022/C_part4_forwhileloop.c
+++ 20241022/C_part4_forwhileloop.c
@@ -1,10 +1,10 @@
 #include<stdio.h>
 
 int main(){
-    int i ;             //迴圈變數
-    int begin = 0 ;     //起始值
-    int end = 10;       //結束的值
-    int increase = 1  ; //遞增或遞減的值
+    // int i ;             //迴圈變數
+    // int begin = 0 ;     //起始值
+    // int end = 10;       //結束的值
+    // int increase = 1  ; //遞增或遞減的值
     // for(i = begin ; i < end ; i += increase ){  //for迴圈，從begin跑到end，可是不含end，因為當 i = end 的時候就結束了 
     //     printf("i = %d now.\n", i );
     // }
@@ -40,6 +40,4 @@ int main(){
         }
         printf("\n");
     }
-
-    
 }
